c93025496a5e081d9bbf794f1ef436e8b387b513
TeamDlt <93529114+TeamDlt@users.noreply.github.com>
2021-11-04 14:46:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from pyrogram import Client as Bot
 
-from callsmusic import run
+from callsmusic.callmusic import run
 from config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
 from handlers import __version__
 
