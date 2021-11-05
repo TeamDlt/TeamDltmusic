492afb4e1fa31dc8df2f19915fb71c7caad85684
TeamDlt <93529114+TeamDlt@users.noreply.github.com>
2021-11-05 19:20:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,27 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+ASSISTANT_USERNAME = getenv("ASSISTANT_USERNAME")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "teamDlt_update")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "teamDlt")
+BOT_USERNAME = getenv("BOT_USERNAME")
+BG_IMAGE = getenv("BG_IMAGE", "https://te.legra.ph/file/1b5f32e7b440302ac6435.png")
+THUMB_IMG = getenv("THUMB_IMG", "https://te.legra.ph/file/1b5f32e7b440302ac6435.png")
+BOT_IMG = getenv("BOT_IMG", "https://te.legra.ph/file/1b5f32e7b440302ac6435.png")
+AUD_IMG = getenv("AUD_IMG", "https://te.legra.ph/file/1b5f32e7b440302ac6435.png")
+QUE_IMG = getenv("QUE_IMG", "https://te.legra.ph/file/1b5f32e7b440302ac6435.png")
 
+admins = {}
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
+
+OWNER_ID = int(getenv("OWNER_ID"))
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "70"))
 
