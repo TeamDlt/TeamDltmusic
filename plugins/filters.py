25bffb2902ec4176df8a7d44bf3b67069fd2fe6c
TeamDlt <93529114+TeamDlt@users.noreply.github.com>
2021-11-08 20:38:44 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -3,7 +3,7 @@ from typing import Union
 
 from pyrogram import filters
 
-from VCPlayBot.config import COMMAND_PREFIXES
+from config import COMMAND_PREFIXES
 
 other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
 other_filters2 = (
